53c6e3d7187b07b6306ec7a9776e7575740e16bf
BuildTools <unconfigured@null.spigotmc.org>
2024-01-21 20:05:59 +0900

    Initial commit

---
+++ src/main/java/net/nanairodev/knana/nanaproxy/Events.java
@@ -0,0 +1,87 @@
+package net.nanairodev.knana.nanaproxy;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.*;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+import net.md_5.bungee.event.EventHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Events implements Listener {
+    private final Configuration config;
+    private final HashMap<String, Configuration> langs = new HashMap<>();
+    public Events(NanaProxy plugin) {
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(plugin.getDataFolder(), "config.yml"));
+            File lFolder = new File(plugin.getDataFolder(), "lang");
+            for (File lFile : Objects.requireNonNull(lFolder.listFiles())) {
+                langs.put(lFile.getName().split("\\.(?=[^\\.]+$)")[0], ConfigurationProvider.getProvider(YamlConfiguration.class).load(lFile));
+                ProxyServer.getInstance().getLogger().info(String.format("[NanaProxy] Loaded Lang: %s", lFile.getName().split("\\.(?=[^\\.]+$)")[0]));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getLocaleMessage(String key, ProxiedPlayer player, String server) {
+        String msg = "";
+        Configuration lang = langs.get(config.getString("Language"));
+        msg = String.format(lang.getString(key), player.getName(), server);
+        return msg;
+    }
+
+    public void broadcast(String msg) {
+        TextComponent tc = new TextComponent(msg);
+        for (ProxiedPlayer lp : ProxyServer.getInstance().getPlayers()) {
+            lp.sendMessage(tc);
+        }
+    }
+
+    @EventHandler
+    public void onPostLogin(PostLoginEvent event) {
+        if (config.getBoolean("LogMessages.Enable")) {
+            ProxyServer.getInstance().getLogger().info(getLocaleMessage("LogMessages.JoinNetwork", event.getPlayer(), ""));
+        }
+        if (config.getBoolean("PlayerMessages.Enable")) {
+            broadcast(getLocaleMessage("BroadcastMessages.JoinNetwork", event.getPlayer(), ""));
+        }
+    }
+
+    @EventHandler
+    public void onDisconnect(PlayerDisconnectEvent event) {
+        if (config.getBoolean("LogMessages.Enable")) {
+            ProxyServer.getInstance().getLogger().info(getLocaleMessage("LogMessages.LeaveNetwork", event.getPlayer(), ""));
+        }
+        if (config.getBoolean("PlayerMessages.Enable")) {
+            broadcast(getLocaleMessage("BroadcastMessages.LeaveNetwork", event.getPlayer(), ""));
+        }
+    }
+
+    @EventHandler
+    public void onSvrConnect(ServerConnectedEvent event) {
+        if (config.getBoolean("LogMessages.Enable")) {
+            ProxyServer.getInstance().getLogger().info(getLocaleMessage("LogMessages.MoveServer", event.getPlayer(), event.getServer().getInfo().getName()));
+        }
+        if (config.getBoolean("PlayerMessages.Enable")) {
+            broadcast(getLocaleMessage("BroadcastMessages.MoveServer", event.getPlayer(), event.getServer().getInfo().getName()));
+        }
+    }
+
+    @EventHandler
+    public void onSvrKick(ServerKickEvent event) {
+        if ((config.getBoolean("LobbyServer.SendOnKick") && (!event.getKickedFrom().getName().equals(config.getString("LobbyServer.ServerID"))))) {
+            TextComponent msg = new TextComponent(getLocaleMessage("BroadcastMessages.MoveServer", event.getPlayer(), config.getString("LobbyServer.ServerID")));
+            event.getPlayer().sendMessage(msg);
+            event.setCancelServer(ProxyServer.getInstance().getServerInfo(config.getString("LobbyServer.ServerID")));
+            event.setCancelled(true);
+        }
+    }
+}
---
+++ src/main/java/net/nanairodev/knana/nanaproxy/NanaProxy.java
@@ -0,0 +1,60 @@
+package net.nanairodev.knana.nanaproxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public final class NanaProxy extends Plugin {
+    private Configuration config = null;
+
+    public void makeConfig(String fName) throws IOException {
+        File file = new File(getDataFolder(), fName);
+
+        if (!file.exists()) {
+            try (InputStream in = getResourceAsStream(fName)) {
+                Files.copy(in, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        ProxyServer.getInstance().getLogger().info("Loading NanaProxy...");
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdir();
+            }
+            File lFolder = new File(getDataFolder(), "lang");
+            if (!lFolder.exists()) {
+                lFolder.mkdir();
+            }
+            makeConfig("config.yml");
+            makeConfig("lang/ja_jp.yml");
+            makeConfig("lang/en_us.yml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new Events(this));
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new Lobby(this));
+        Metrics metrics = new Metrics(this, 20738);
+        metrics.addCustomChart(new Metrics.SimplePie("used_language", () -> config.getString("Language", "unknown")));
+        ProxyServer.getInstance().getLogger().info("[NanaProxy] Successful Loaded NanaProxy!");
+    }
+
+    @Override
+    public void onDisable() {
+        ProxyServer.getInstance().getLogger().info("[NanaProxy] Successful Unloaded NanaProxy!");
+    }
+}
